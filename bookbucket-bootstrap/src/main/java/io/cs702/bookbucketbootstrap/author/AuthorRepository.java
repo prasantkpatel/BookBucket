7c2475a33821ee322c6f535ac47137413d9ecd6f
Prasant Kumar Patel <mania.prashant1@gmail.com>
2021-10-31 21:09:44 +0530

    Initial Commit

--- bookbucket-bootstrap/src/main/java/io/cs702/bookbucketbootstrap/author/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package io.cs702.bookbucketbootstrap.author;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-public interface AuthorRepository extends CassandraRepository<Author, String> {
-    
-}
--- bookbucket-bootstrap/src/main/java/io/cs702/bookbucketbootstrap/book/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package io.cs702.bookbucketbootstrap.book;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-public interface BookRepository extends CassandraRepository<Book, String> {
-    
-}
